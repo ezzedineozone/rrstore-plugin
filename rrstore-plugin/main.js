f951a5083de4f34d6614198ecade6e7bb3f12ebd
Ezzedine <ezzedineozone123@gmail.com>
2024-09-29 17:26:42 +0300

    added mobile navigation menu for respnsiveness

--- rrstore-plugin/main.js
+++ rrstore-plugin/main.js
@@ -2,6 +2,7 @@ var baseUrl = wpData.baseUrl;
 var cartUrl = baseUrl + "/" + "cart/";
 var productsUrl = baseUrl + "/" +"products/";
 var aboutusUrl = baseUrl + "/" + "about-us/";
+var mobile_modal_open = false;
 
 function onCartButtonClicked(){
     window.location.href = cartUrl;
@@ -15,3 +16,56 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+jQuery(document).ready(($)=>{
+    function handleHeaderResponsiveness(){
+        let min_width_to_display_nav = $('.navigation-container').outerWidth(true) +$('.logo-container').outerWidth(true) + 2*parseInt($('.header').css('padding-left'))+10;
+        function mainHandleResponsiveness(){
+            if($(window).width() < min_width_to_display_nav)
+            {
+                $('.navigation-container').hide();
+                $('#mobile-navigation-button-container').show();
+            }
+            else
+            {
+                $('#mobile-navigation-button-container').hide();
+                $('.navigation-container').show();
+            }
+        }
+        $(window).resize(function() 
+        {
+            mainHandleResponsiveness();
+        });
+        mainHandleResponsiveness();
+    }
+    function setMainEventHandlers(){
+        $('.mobile-navigation-button').click(()=>{
+            if(mobile_modal_open == false)
+            {
+                $('#mobile-navigation-modal').show();
+                mobile_modal_open = true;
+            }
+            else 
+            {
+                $('#mobile-navigation-modal').hide();
+                mobile_modal_open = false;
+            }
+        });
+        // Handle dropdown toggle for menu items with children
+        $('.menu-item-has-children > a').click(function(e) {
+            e.preventDefault(); // Prevent default link behavior
+
+            const parentLi = $(this).parent();
+            
+            // Toggle the 'menu-item-active' class on click
+            if (parentLi.hasClass('menu-item-active')) {
+                parentLi.removeClass('menu-item-active');
+                parentLi.find('.sub-menu').slideUp(); // Hide the sub-menu
+            } else {
+                parentLi.addClass('menu-item-active');
+                parentLi.find('.sub-menu').slideDown(); // Show the sub-menu
+            }
+        });
+    }
+    handleHeaderResponsiveness();
+    setMainEventHandlers();
+});
--- rrstore-plugin/templates/custom_header.php
+++ rrstore-plugin/templates/custom_header.php
@@ -9,6 +9,38 @@
 <body class = "body"> 
 <div class = "header">
     <img class = "logo-container" src="<?php echo plugin_dir_url(__FILE__); ?>../images/rr-logo.png"/>
+    <div id ="mobile-navigation-button-container" hidden>
+        <button class="rounded-md flex flex-col items-center  border-black border-1 mobile-navigation-button" id="mobile-navigation-button">
+            <svg class="w-8 h-8 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5" />
+            </svg>
+            <p class = "font-semibold" >Menu</p>
+        </button>
+        <div class = "absolute top-0 left-0 w-full h-full bg-yellow-custom z-50" id = "mobile-navigation-modal" hidden>
+            <div id = "mobile-navigation-container" class = "mobile-navigation-container">
+                <div class = "w-full h-max flex flex-row justify-end">
+                    <button class="rounded-md flex flex-col items-center  border-black border-1 mobile-navigation-button" >
+                        <svg class="w-6 h-6 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/>
+                        </svg>
+                        <p class = "font-semibold" >Close Menu</p>
+                    </button>
+                </div>
+                <div class = "mobile-navigation">
+                    <?php 
+                        wp_nav_menu(
+                            array(
+                                'menu' => 'primary',
+                                'container' => '',
+                                'theme_location' => 'primary',
+                                'items_wrap'=> '<ul id="%1$s" class="%2$s mobile-navigation">%3$s</ul>',
+                            )
+                        );
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class = "navigation-container">
         <?php 
             wp_nav_menu(
@@ -21,7 +53,7 @@
             );
         ?>
         <div class = "custom-navigation-container">
-            <button class = "product-card-actions-cart-header" onClick="onCartButtonClicked()">
+            <button class = "product-card-actions-cart-header " onClick="onCartButtonClicked()">
                     <svg class="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/>
                     </svg>
